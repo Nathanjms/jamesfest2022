c2083680725ed2a6e09b86d9525ae8d49e914c48
Nathan James <nathanjms@outlook.com>
2022-04-11 21:59:17 +0100

    Overlay

--- components/CustomNavbar.js
+++ components/CustomNavbar.js
@@ -10,32 +10,30 @@ import {
 export const CustomNavbar = () => {
   return (
     <>
-      <Navbar collapseOnSelect expand="md" bg="light" variant="light">
-        <Container>
-          <Navbar.Brand href="#home">
-            <Image
-              alt="JamesFest"
-              src="/JamesFestText.png"
-              width={100}
-              height={20}
-              className="d-inline-block align-top"
-            />
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="offcanvasNavbar" />
-          <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link href="#features">Features</Nav.Link>
-              <Nav.Link href="#pricing">Pricing</Nav.Link>
-            </Nav>
-            <Nav>
-              <Nav.Link href="#deets">More deets</Nav.Link>
-              <Nav.Link eventKey={2} href="#memes">
-                Dank memes
-              </Nav.Link>
-            </Nav>
-          </Navbar.Collapse>
-        </Container>
-      </Navbar>
+      <div id="myNav" className="overlay">
+        <a href="javascript:void(0)" className="closebtn" onClick={() => closeNav()}>
+          &times;
+        </a>
+
+        <div className="overlay-content">
+          <a href="#">About</a>
+          <a href="#">Services</a>
+          <a href="#">Clients</a>
+          <a href="#">Contact</a>
+        </div>
+      </div>
+
+      <button className="btn btn-danger" id="mobileNavBtn" onClick={() => openNav()}>open</button>
     </>
   );
 };
+
+/* Open when someone clicks on the span element */
+function openNav() {
+  document.getElementById("myNav").style.height = "100%";
+}
+
+/* Close when someone clicks on the "x" symbol inside the overlay */
+function closeNav() {
+  document.getElementById("myNav").style.height = "0%";
+} 
