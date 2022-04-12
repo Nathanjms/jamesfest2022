dee7ceb735d65ea2a71e23b1653a1d14003c232a
Nathan James <nathanjms@outlook.com>
2022-04-12 21:43:38 +0100

    minor naming changes

--- components/CustomNavbar.js
+++ components/CustomNavbar.js
@@ -6,7 +6,7 @@ export const CustomNavbar = () => {
   const [overlayOpen, setOverlayOpen] = useState(false);
 
   const toggleOverlay = () => {
-    let overlay = document.querySelector("#myNav");
+    let overlay = document.querySelector("#navOverlay");
     let mobileNavBtn = document.querySelector("#mobileNavBtn");
     // Disable button during transition
     mobileNavBtn.disabled = true;
@@ -14,28 +14,28 @@ export const CustomNavbar = () => {
       mobileNavBtn.disabled = false;
     }, 400);
     if (overlayOpen) {
-      closeNav(overlay);
+      closeOverlay(overlay);
     } else {
-      openNav(overlay);
+      openOverlay(overlay);
     }
     setOverlayOpen(!overlayOpen);
   };
   /* Open when someone clicks on the span element */
-  function openNav(overlay) {
+  function openOverlay(overlay) {
     overlay.style.height = "100%";
     document.querySelector("body").classList.add("overlay-open");
   }
 
   /* Close when someone clicks on the "x" symbol inside the overlay */
-  function closeNav(overlay) {
+  function closeOverlay(overlay) {
     overlay.style.height = "0%";
     document.querySelector("body").classList.remove("overlay-open");
   }
 
   return (
     <>
-      <div id="myNav" className="overlay">
-        <a href="#" className="closebtn" onClick={toggleOverlay}>
+      <div id="navOverlay" className="overlay">
+        <a href="#" className="closeBtn" id="overlayCloseBtn" tabIndex={0} onClick={toggleOverlay}>
           &times;
         </a>
         <div className="pt-5 text-center">
@@ -49,10 +49,10 @@ export const CustomNavbar = () => {
 
         <div className="overlay-content text-white">
           {/* <h3>JamesFest</h3> */}
-          <a href="#">About</a>
-          <a href="#">Services</a>
-          <a href="#">Clients</a>
-          <a href="#">Contact</a>
+          <a href="#about">About</a>
+          <a href="#services">Services</a>
+          <a href="#clients">Clients</a>
+          <a href="#contacts">Contact</a>
         </div>
         <button
           className="btn btn-primary"
