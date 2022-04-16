b7af684d0cb4f5bbcae1b594012c5f87feb0d74f
Nathan James <nathanjms@outlook.com>
2022-04-16 16:17:57 +0100

    Do layout per-page

--- pages/_app.js
+++ pages/_app.js
@@ -2,12 +2,9 @@ import "bootstrap/dist/css/bootstrap.css";
 import "animate.css";
 import "../styles/globals.css";
 import "../styles/navbar.css";
-import DefaultLayout from "../components/layouts/default";
 
-export default function MyApp({ Component, pageProps }) {
-  return (
-    <DefaultLayout>
-      <Component {...pageProps} />
-    </DefaultLayout>
-  );
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
 }
+
+export default MyApp;
