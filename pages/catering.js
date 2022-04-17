f4f699f7f6b6e95f53934100597974337460eb17
Nathan James <nathanjms@outlook.com>
2022-04-17 09:36:53 +0100

    DefaultLayout

--- pages/catering.js
+++ pages/catering.js
@@ -1,15 +1,18 @@
+import DefaultLayout from "../components/layouts/DefaultLayout";
 import { getUserFromServerSession } from "../lib/withSession";
 
 export const getServerSideProps = getUserFromServerSession({
   redirectToLogin: true,
-  redirectUrl: 'catering',
+  redirectUrl: "catering",
 });
 
 export default function Catering({ user }) {
   return (
-    <div>
-      <h1>Catering</h1>
-      <h2>{user.authenticated}</h2>
-    </div>
+    <DefaultLayout>
+      <div id="#catering" className="section">
+        <h1>Catering</h1>
+        <h2>{user.authenticated}</h2>
+      </div>
+    </DefaultLayout>
   );
 }
