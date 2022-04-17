81166f8a5ebee68ae6c308817a56f54ac770dc1e
Nathan James <nathanjms@outlook.com>
2022-04-17 09:01:50 +0100

    Default layout optional param

--- components/layouts/DefaultLayout.js
+++ components/layouts/DefaultLayout.js
@@ -1,7 +1,7 @@
 import Header from "./header";
 import Footer from "./footer";
 
-export default function DefaultLayout({ children, props, title }) {
+export default function DefaultLayout({ children, props, title = '' }) {
   return (
     <>
       <Header title={title} />
