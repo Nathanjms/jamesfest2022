8be1dab63079400608b9d7b135328221a13a160d
Nathan James <nathanjms@outlook.com>
2022-04-18 22:43:07 +0100

    Also add to homepage

--- pages/index.js
+++ pages/index.js
@@ -2,11 +2,14 @@ import Head from "next/head";
 import Image from "next/image";
 import DefaultLayout from "../components/layouts/DefaultLayout";
 import { TimerDisplay } from "../components/TimerDisplay";
+import { FaIceCream, FaMusic } from "react-icons/fa";
+import { GiTeapot } from "react-icons/gi";
+import { MdOutlinePark, MdSportsCricket } from "react-icons/md";
 
 const weddingDate = new Date(1657926000000);
 export default function Home() {
   return (
-    <DefaultLayout footerAlt={true}>
+    <DefaultLayout>
       <section id="intro" className="section intro">
         <div className="container text-center">
           <div className="row">
@@ -81,46 +84,81 @@ export default function Home() {
       </section>
       <section className="section">
         <div className="container text-center">
-          <div className="row">
-            <h2 className="h3 mb-3">
-              Here&apos;s what we&apos;ve got planned (at the moment)
-            </h2>
-            <div className="col-md-12">
-              <p>
-                Firstly the venue, we think you’ll love it. We chose the river
-                school as we liked the beautiful trees, natural grounds and
-                wonderful countryside views. Back in the 1930’s it was a
-                horticultural college which explains why so many lovely and
-                unusual trees can be found here... In fact, we have chosen a
-                &quot;special&quot; tree, under which, we intend to hold our
-                wedding ceremony, with you all very close by, lounging on hay
-                bales.
-              </p>
-              <p>
-                After the ceremony, you can relax or take a wander around the
-                grounds while enjoying reception drinks, followed closely by an
-                afternoon tea in our magical tipi.
-              </p>
-              <p>
-                We have a great afternoon planned for you; a series of musical
-                acts throughout the day, along with heaps of communal games.
-                Let’s try to break the record for the most people playing in a
-                rounder’s game!!
-              </p>
-              <p>
-                We’ll have an antique ice cream cart on duty throughout the
-                afternoon and of course an outdoor bar. In the early evening, we
-                will have street food available outside the tipi.
-              </p>
-              <p>
-                We are planning a mammoth communal sing-along around fire-pits
-                while the sun is setting, before dancing all night long to the
-                fabulous Ellie Jones and the Giants.
-              </p>
-              <p>
-                With all this in mind, please keep your fingers and everything
-                else crossed for good weather.
-              </p>
+          <h2 className="h3 mb-3">
+            Here&apos;s what we&apos;ve got planned (at the moment)
+          </h2>
+          <div className="row justify-content-center">
+            <div className="col-md-12 whatsOnCardContainer">
+              <div className="card h-100 whatsOnCard">
+                <h3 className="h1">
+                  <MdOutlinePark />
+                </h3>
+                <p className="ps-1 pe-1">
+                  Firstly the venue, we think you’ll love it. We chose the river
+                  school as we liked the beautiful trees, natural grounds and
+                  wonderful countryside views. Back in the 1930’s it was a
+                  horticultural college which explains why so many lovely and
+                  unusual trees can be found here... In fact, we have chosen a
+                  &quot;special&quot; tree, under which, we intend to hold our
+                  wedding ceremony, with you all very close by, lounging on hay
+                  bales.
+                </p>
+              </div>
+            </div>
+            <div className="col-md-6 whatsOnCardContainer">
+              <div className="card h-100 whatsOnCard">
+                <h3 className="h1">
+                  <GiTeapot />
+                </h3>
+                <p className="ps-1 pe-1">
+                  After the ceremony, you can relax or take a wander around the
+                  grounds while enjoying reception drinks, followed closely by
+                  an afternoon tea in our magical tipi.
+                </p>
+              </div>
+            </div>
+            <div className="col-md-6 whatsOnCardContainer">
+              <div className="card h-100 whatsOnCard">
+                <h3 className="h1">
+                  <MdSportsCricket />
+                </h3>
+                <p className="ps-1 pe-1">
+                  We have a great afternoon planned for you; a series of musical
+                  acts throughout the day, along with heaps of communal games.
+                  Let’s try to break the record for the most people playing in a
+                  rounder’s game!!
+                </p>
+              </div>
+            </div>
+            <div className="col-md-6 whatsOnCardContainer">
+              <div className="card h-100 whatsOnCard">
+                <h3 className="h1">
+                  <FaIceCream />
+                </h3>
+                <p className="ps-1 pe-1">
+                  We’ll have an antique ice cream cart on duty throughout the
+                  afternoon and of course an outdoor bar. In the early evening,
+                  we will have street food available outside the tipi.
+                </p>
+              </div>
+            </div>
+            <div className="col-md-6 whatsOnCardContainer">
+              <div className="card h-100 whatsOnCard">
+                <h3 className="h1">
+                  <FaMusic />
+                </h3>
+                <p className="ps-1 pe-1">
+                  We are planning a mammoth communal sing-along around fire-pits
+                  while the sun is setting, before dancing all night long to the
+                  fabulous Ellie Jones and the Giants.
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="row justify-content-center pt-5">
+            <div className="col-12">
+              With all this in mind, please keep your fingers and everything
+              else crossed for good weather.
             </div>
           </div>
         </div>
--- pages/whats-on.js
+++ pages/whats-on.js
@@ -17,14 +17,16 @@ export default function WhatsOn() {
                 <h3 className="h1">
                   <MdOutlinePark />
                 </h3>
-                Firstly the venue, we think you’ll love it. We chose the river
-                school as we liked the beautiful trees, natural grounds and
-                wonderful countryside views. Back in the 1930’s it was a
-                horticultural college which explains why so many lovely and
-                unusual trees can be found here... In fact, we have chosen a
-                &quot;special&quot; tree, under which, we intend to hold our
-                wedding ceremony, with you all very close by, lounging on hay
-                bales.
+                <p className="ps-1 pe-1">
+                  Firstly the venue, we think you’ll love it. We chose the river
+                  school as we liked the beautiful trees, natural grounds and
+                  wonderful countryside views. Back in the 1930’s it was a
+                  horticultural college which explains why so many lovely and
+                  unusual trees can be found here... In fact, we have chosen a
+                  &quot;special&quot; tree, under which, we intend to hold our
+                  wedding ceremony, with you all very close by, lounging on hay
+                  bales.
+                </p>
               </div>
             </div>
             <div className="col-md-6 whatsOnCardContainer">
@@ -32,9 +34,11 @@ export default function WhatsOn() {
                 <h3 className="h1">
                   <GiTeapot />
                 </h3>
-                After the ceremony, you can relax or take a wander around the
-                grounds while enjoying reception drinks, followed closely by an
-                afternoon tea in our magical tipi.
+                <p className="ps-1 pe-1">
+                  After the ceremony, you can relax or take a wander around the
+                  grounds while enjoying reception drinks, followed closely by
+                  an afternoon tea in our magical tipi.
+                </p>
               </div>
             </div>
             <div className="col-md-6 whatsOnCardContainer">
@@ -42,10 +46,12 @@ export default function WhatsOn() {
                 <h3 className="h1">
                   <MdSportsCricket />
                 </h3>
-                We have a great afternoon planned for you; a series of musical
-                acts throughout the day, along with heaps of communal games.
-                Let’s try to break the record for the most people playing in a
-                rounder’s game!!
+                <p className="ps-1 pe-1">
+                  We have a great afternoon planned for you; a series of musical
+                  acts throughout the day, along with heaps of communal games.
+                  Let’s try to break the record for the most people playing in a
+                  rounder’s game!!
+                </p>
               </div>
             </div>
             <div className="col-md-6 whatsOnCardContainer">
@@ -53,9 +59,11 @@ export default function WhatsOn() {
                 <h3 className="h1">
                   <FaIceCream />
                 </h3>
-                We’ll have an antique ice cream cart on duty throughout the
-                afternoon and of course an outdoor bar. In the early evening, we
-                will have street food available outside the tipi.
+                <p className="ps-1 pe-1">
+                  We’ll have an antique ice cream cart on duty throughout the
+                  afternoon and of course an outdoor bar. In the early evening,
+                  we will have street food available outside the tipi.
+                </p>
               </div>
             </div>
             <div className="col-md-6 whatsOnCardContainer">
@@ -63,14 +71,16 @@ export default function WhatsOn() {
                 <h3 className="h1">
                   <FaMusic />
                 </h3>
-                We are planning a mammoth communal sing-along around fire-pits
-                while the sun is setting, before dancing all night long to the
-                fabulous Ellie Jones and the Giants.
+                <p className="ps-1 pe-1">
+                  We are planning a mammoth communal sing-along around fire-pits
+                  while the sun is setting, before dancing all night long to the
+                  fabulous Ellie Jones and the Giants.
+                </p>
               </div>
             </div>
           </div>
-          <div className="row justify-content-center pt-3">
-            <div className="col-12 whatsOnCardContainer">
+          <div className="row justify-content-center pt-5">
+            <div className="col-12">
               With all this in mind, please keep your fingers and everything
               else crossed for good weather.
             </div>
