ed9054bdf8a049ee6e6b973102619ca7d51fffed
Nathan James <nathanjms@outlook.com>
2022-04-18 22:44:26 +0100

    FooterAlt colour

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ import { MdOutlinePark, MdSportsCricket } from "react-icons/md";
 const weddingDate = new Date(1657926000000);
 export default function Home() {
   return (
-    <DefaultLayout>
+    <DefaultLayout footerAlt={true}>
       <section id="intro" className="section intro">
         <div className="container text-center">
           <div className="row">
