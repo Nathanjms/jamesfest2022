cfdfe6fe333ecfeb75e9db4b49673018774cadc2
Nathan James <nathanjms@outlook.com>
2022-04-19 15:19:21 +0100

    Add whatson to homepage, remove separate page

--- components/WhatsOn.js
+++ components/WhatsOn.js
@@ -1,24 +1,21 @@
 import { FaIceCream, FaMusic } from "react-icons/fa";
-import { GiTeapot } from "react-icons/gi";
-import { MdOutlinePark, MdSportsCricket } from "react-icons/md";
-import DefaultLayout from "../components/layouts/DefaultLayout";
+import { GiMicrophone, GiTeapot } from "react-icons/gi";
+import { MdOutlinePark } from "react-icons/md";
 
 export default function WhatsOn() {
   return (
-    <DefaultLayout footerAlt={true}>
-      <section className="section">
-        <div className="container text-center">
-          <h2 className="h3 mb-3">
-            Here&apos;s what we&apos;ve got planned (at the moment)
-          </h2>
-          <div className="row justify-content-center">
-            {whatsOnContent.map((content, index) => {
-              return <WhatsOnCard content={content} key={index} />;
-            })}
-          </div>
+    <section className="section">
+      <div className="container text-center">
+        <h2 className="h3 mb-3">
+          Here&apos;s what we&apos;ve got planned (at the moment)
+        </h2>
+        <div className="row justify-content-center">
+          {whatsOnContent.map((content, index) => {
+            return <WhatsOnCard content={content} key={index} />;
+          })}
         </div>
-      </section>
-    </DefaultLayout>
+      </div>
+    </section>
   );
 }
 
@@ -42,10 +39,10 @@ const whatsOnContent = [
     icon: <MdOutlinePark />,
     text: (
       <>
-        Firstly the venue, we think you’ll love it. The venue has beautiful
+        Firstly the venue, we think you&apos;ll love it. The venue has beautiful
         trees, natural grounds and wonderful countryside views. Back in the
-        1930’s it was a horticultural college which explains why so many lovely
-        and unusual trees can be found here... In fact, we have chosen a
+        1930&apos;s it was a horticultural college which explains why so many
+        lovely and unusual trees can be found here... In fact, we have chosen a
         &quot;special&quot; tree, under which, we intend to hold our wedding
         ceremony, with you all very close by, lounging on hay bales.
       </>
@@ -65,12 +62,13 @@ const whatsOnContent = [
     fullWidth: false,
   },
   {
-    icon: <MdSportsCricket />,
+    icon: <FaMusic />,
     text: (
       <>
         We have a great afternoon planned for you; a series of musical acts
-        throughout the day, along with heaps of communal games. Let’s try to
-        break the record for the most people playing in a rounder’s game!!
+        throughout the day, along with heaps of communal games. Let&apos;s try
+        to break the record for the most people playing in a rounder&apos;s
+        game!!
       </>
     ),
     fullWidth: false,
@@ -79,15 +77,15 @@ const whatsOnContent = [
     icon: <FaIceCream />,
     text: (
       <>
-        We’ll have an antique ice cream cart on duty throughout the afternoon
-        and of course an outdoor bar. In the early evening, we will have street
-        food available outside the tipi.
+        We&apos;ll have an antique ice cream cart on duty throughout the
+        afternoon and of course an outdoor bar. In the early evening, we will
+        have street food available outside the tipi.
       </>
     ),
     fullWidth: false,
   },
   {
-    icon: <FaMusic />,
+    icon: <GiMicrophone />,
     text: (
       <>
         We are planning a mammoth communal sing-along around fire-pits while the
