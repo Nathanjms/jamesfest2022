eb76b883f41342ca6e5a606b3304722b5a0983d8
Nathan James <nathanjms@outlook.com>
2022-04-19 09:26:15 +0100

    Streamline whatsOnCards

--- pages/whats-on.js
+++ pages/whats-on.js
@@ -12,81 +12,74 @@ export default function WhatsOn() {
             Here&apos;s what we&apos;ve got planned (at the moment)
           </h2>
           <div className="row justify-content-center">
-            <div className="col-md-12 whatsOnCardContainer">
-              <div className="card h-100 whatsOnCard">
-                <h3 className="h1">
-                  <MdOutlinePark />
-                </h3>
-                <p className="ps-1 pe-1">
-                  Firstly the venue, we think you’ll love it. We chose the river
-                  school as we liked the beautiful trees, natural grounds and
-                  wonderful countryside views. Back in the 1930’s it was a
-                  horticultural college which explains why so many lovely and
-                  unusual trees can be found here... In fact, we have chosen a
-                  &quot;special&quot; tree, under which, we intend to hold our
-                  wedding ceremony, with you all very close by, lounging on hay
-                  bales.
-                </p>
-              </div>
-            </div>
-            <div className="col-md-6 whatsOnCardContainer">
-              <div className="card h-100 whatsOnCard">
-                <h3 className="h1">
-                  <GiTeapot />
-                </h3>
-                <p className="ps-1 pe-1">
-                  After the ceremony, you can relax or take a wander around the
-                  grounds while enjoying reception drinks, followed closely by
-                  an afternoon tea in our magical tipi.
-                </p>
-              </div>
-            </div>
-            <div className="col-md-6 whatsOnCardContainer">
-              <div className="card h-100 whatsOnCard">
-                <h3 className="h1">
-                  <MdSportsCricket />
-                </h3>
-                <p className="ps-1 pe-1">
-                  We have a great afternoon planned for you; a series of musical
-                  acts throughout the day, along with heaps of communal games.
-                  Let’s try to break the record for the most people playing in a
-                  rounder’s game!!
-                </p>
-              </div>
-            </div>
-            <div className="col-md-6 whatsOnCardContainer">
-              <div className="card h-100 whatsOnCard">
-                <h3 className="h1">
-                  <FaIceCream />
-                </h3>
-                <p className="ps-1 pe-1">
-                  We’ll have an antique ice cream cart on duty throughout the
-                  afternoon and of course an outdoor bar. In the early evening,
-                  we will have street food available outside the tipi.
-                </p>
-              </div>
-            </div>
-            <div className="col-md-6 whatsOnCardContainer">
-              <div className="card h-100 whatsOnCard">
-                <h3 className="h1">
-                  <FaMusic />
-                </h3>
-                <p className="ps-1 pe-1">
-                  We are planning a mammoth communal sing-along around fire-pits
-                  while the sun is setting, before dancing all night long to the
-                  fabulous Ellie Jones and the Giants.
-                </p>
-              </div>
-            </div>
-          </div>
-          <div className="row justify-content-center pt-5">
-            <div className="col-12">
-              With all this in mind, please keep your fingers and everything
-              else crossed for good weather.
-            </div>
+            {whatsOnContent.map((content, index) => {
+              return <WhatsOnCard content={content} key={index} />;
+            })}
           </div>
         </div>
       </section>
     </DefaultLayout>
   );
 }
+
+const WhatsOnCard = ({ content }) => {
+  return (
+    <div
+      className={
+        "whatsOnCardContainer col-md-" + (content.fullWidth ? "12" : "6")
+      }
+    >
+      <div className="card h-100 whatsOnCard">
+        <h3 className="h1">{content.icon}</h3>
+        <p className="ps-1 pe-1">{content.text}</p>
+      </div>
+    </div>
+  );
+};
+
+const whatsOnContent = [
+  {
+    icon: <MdOutlinePark />,
+    text: `Firstly the venue, we think you’ll love it. We chose the river
+  school as we liked the beautiful trees, natural grounds and
+  wonderful countryside views. Back in the 1930’s it was a
+  horticultural college which explains why so many lovely and
+  unusual trees can be found here... In fact, we have chosen a
+  &quot;special&quot; tree, under which, we intend to hold our
+  wedding ceremony, with you all very close by, lounging on hay
+  bales.`,
+    fullWidth: true,
+  },
+
+  {
+    icon: <GiTeapot />,
+    text: `After the ceremony, you can relax or take a wander around the
+  grounds while enjoying reception drinks, followed closely by
+  an afternoon tea in our magical tipi.`,
+    fullWidth: false,
+  },
+  {
+    icon: <MdSportsCricket />,
+    text: `We have a great afternoon planned for you; a series of musical
+  acts throughout the day, along with heaps of communal games.
+  Let’s try to break the record for the most people playing in a
+  rounder’s game!!`,
+    fullWidth: false,
+  },
+  {
+    icon: <FaIceCream />,
+    text: `We’ll have an antique ice cream cart on duty throughout the
+  afternoon and of course an outdoor bar. In the early evening,
+  we will have street food available outside the tipi.`,
+    fullWidth: false,
+  },
+  {
+    icon: <FaMusic />,
+    text: `We are planning a mammoth communal sing-along around fire-pits
+  while the sun is setting, before dancing all night long to the
+  fabulous Ellie Jones and the Giants.`,
+    fullWidth: false,
+  },
+];
+
+export { WhatsOnCard, whatsOnContent };
