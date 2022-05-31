59d290b70112b71a66e1ec16f2b1a83a9f1af363
Nathan James <nathanjms@outlook.com>
2022-05-31 21:16:17 +0100

    new news item

--- components/newsItems.js
+++ components/newsItems.js
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import Link from "next/link";
 
 export const newsItems = [
@@ -6,7 +7,10 @@ export const newsItems = [
     date: "25th May 2022",
     content: (
       <>
-        <p>We will update this section with every new change to the website - be sure to check back here to stay updated on any website changes!</p>
+        <p>
+          We will update this section with every new change to the website - be
+          sure to check back here to stay updated on any website changes!
+        </p>
       </>
     ),
   },
@@ -15,7 +19,42 @@ export const newsItems = [
     date: "25th May 2022",
     content: (
       <>
-        <p>The Music section has been updated with the latest acts that will be playing at JamesFest, <Link href="/music">click here to see the updates!</Link> </p>
+        <p>
+          Introducing the Music section... featuring some of the acts that will
+          be playing at JamesFest, <Link href="/music">click here</Link> to go
+          to this section now!
+        </p>
+      </>
+    ),
+  },
+  {
+    title: "Music Section - Updates",
+    date: "31st May 2022",
+    content: (
+      <>
+        <p>
+          The Music section has been updated with even more of the acts that
+          will be playing at JamesFest! <Link href="/music">Click here</Link> to
+          see the updates!
+        </p>
+        <div className="row justify-items-center mt-4">
+          <div className="col-md-6 text-center mb-3">
+            <Image
+              src="/robotRadio-logo.webp"
+              alt="Robot Radio Logo"
+              height={117}
+              width={245.25}
+            />
+          </div>
+          <div className="col-md-6 text-center mb-3">
+            <Image
+              src="/moselele-logo.webp"
+              alt="Moselele Logo"
+              height={117}
+              width={245.25}
+            />
+          </div>
+        </div>
       </>
     ),
   },
