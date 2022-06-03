ef44d3ba4bbed07060c876097ae4b2f6dd867a77
Nathan James <nathanjms@outlook.com>
2022-06-03 20:21:08 +0100

    extract newsItems & add update dates

--- components/CustomNavbar.js
+++ components/CustomNavbar.js
@@ -4,44 +4,7 @@ import { useState } from "react";
 import { Container, Nav, Navbar } from "react-bootstrap";
 import { FaBars, FaTimes } from "react-icons/fa";
 import { useRouter } from "next/router";
-
-export const navItems = [
-  {
-    name: "News",
-    link: "/news",
-    lastUpdated: 1654174800000, // Thu Jun 02 2022 14:00:00 GMT+0100 (British Summer Time)
-  },
-  {
-    name: "Venue",
-    link: "/venue",
-    lastUpdated: 1650910962908, // Mon Apr 25 2022 19:23:10 GMT+0100 (British Summer Time)
-  },
-  {
-    name: "Accommodation",
-    link: "/accommodation",
-    lastUpdated: 1650910962908, // Mon Apr 25 2022 19:23:10 GMT+0100 (British Summer Time)
-  },
-  {
-    name: "Music",
-    link: "/music",
-    lastUpdated: 1654029000000, // Tue May 31 2022 21:30:00 GMT+0100 (British Summer Time)
-  },
-  {
-    name: "Food",
-    link: "/food",
-    lastUpdated: 1650804521371, // Sun Apr 24 2022 13:48:41 GMT+0100 (British Summer Time) 
-  },
-  {
-    name: "Gift List",
-    link: "/gift-list",
-    lastUpdated: 1650804521371, // Sun Apr 24 2022 13:48:41 GMT+0100 (British Summer Time) 
-  },
-  {
-    name: "The Day",
-    link: "/day",
-    lastUpdated: 1654207200000, // Thu Jun 02 2022 23:00:00 GMT+0100 (British Summer Time)
-  },
-];
+import { navItems } from "./data/navItems";
 
 export const CustomNavbar = ({ homeLinkOnly = false }) => {
   const [overlayOpen, setOverlayOpen] = useState(false);
